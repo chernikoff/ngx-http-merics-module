089fa7af05f742f5fa4961bf1118e18ff8b99be1
chernikoff <chernikoff.alex@gmail.com>
2016-11-16 14:54:56 +0300

    Added debug ouput in stderr

--- ngx_http_metrics_module.c
+++ ngx_http_metrics_module.c
@@ -75,6 +75,8 @@ ngx_http_metrics_init(ngx_conf_t *cf)
   ngx_http_handler_pt       *h;
   ngx_http_core_main_conf_t *cmcf;
 
+  fprintf(stderr, "METRICS: call ngx_http_metrics_init");
+
   cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
   h = ngx_array_push(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
@@ -90,24 +92,27 @@ ngx_http_metrics_init(ngx_conf_t *cf)
 static ngx_int_t
 ngx_http_metrics_process_init(ngx_cycle_t *cycle)
 {
+  fprintf(stderr, "METRICS: call ngx_http_metrics_process_init");
   return 0;
 }
 
 static void *
 ngx_http_metrics_create_main_conf(ngx_conf_t *cf)
 {
+  fprintf(stderr, "METRICS: call ngx_http_metrics_create_main_conf");
   return 0;
 }
 
 static char *
 ngx_http_metrics_config(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
+  fprintf(stderr, "METRICS: call ngx_http_metrics_config");
   return 0;
 }
 
 ngx_int_t
 ngx_http_metrics_handler(ngx_http_request_t *r)
 {
+  fprintf(stderr, "METRICS: call ngx_http_metrics_handler");
   return 0;
-  
 }
